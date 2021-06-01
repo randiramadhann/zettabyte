6e106e6f75fcfebc83f1b2ec22ed41b5860399f1
Randi Aulia Ramadhan <randiauliar@gmail.com>
2021-06-01 11:43:28 +0700

    add test js number4

--- JS-test/test4.js
+++ JS-test/test4.js
@@ -1,56 +1,64 @@
-// Expected Result : 
-// [
-//   { name: 'Rahman', school_name: 'STJK' },
-//   { name: 'Budi', school_name: 'STJK' }
-// ]
-// Direction :
-// Return a formatted array for students of specific school using id of school.
-const students = [
-  {
-    id: "1",
-    name: "Ryan",
-    school: {
+  // Expected Result :
+  // [
+  //   { name: 'Rahman', school_name: 'STJK' },
+  //   { name: 'Budi', school_name: 'STJK' }
+  // ]
+  // Direction :
+  // Return a formatted array for students of specific school using id of school.
+  const students = [
+    {
       id: "1",
-      short_name: "PLMK"
+      name: "Ryan",
+      school: {
+        id: "1",
+        short_name: "PLMK",
+      },
     },
-  },
-  {
-    id: "2",
-    name: "Ari",
-    school: {
-      id: "1",
-      short_name: "PLMK"
-    },
-  },
-  {
-    id: "3",
-    name: "Rahman",
-    school: {
+    {
       id: "2",
-      short_name: "STJK"
+      name: "Ari",
+      school: {
+        id: "1",
+        short_name: "PLMK",
+      },
     },
-  },
-  {
-    id: "4",
-    name: "Budi",
-    school: {
-      id: "2",
-      short_name: "STJK"
+    {
+      id: "3",
+      name: "Rahman",
+      school: {
+        id: "2",
+        short_name: "STJK",
+      },
     },
-  },
-  {
-    id: "5",
-    name: "Jaka",
-    school: {
-      id: "1",
-      short_name: "PLMK"
+    {
+      id: "4",
+      name: "Budi",
+      school: {
+        id: "2",
+        short_name: "STJK",
+      },
     },
+    {
+      id: "5",
+      name: "Jaka",
+      school: {
+        id: "1",
+        short_name: "PLMK",
+      },
+    },
+  ];
+  const schoolId = "2";
+
+  function result(students, schoolId) {
+    students.map((item) => {
+      if(item.school.id == schoolId)
+      {
+        return {
+          name: item.name,
+          school_name: item.school.name,
+        };
+      }
+    });
   }
-]
-const schoolId = '2';
 
-function result(students, schoolId) {
-  z
-}
-
-console.log(result(students, schoolId));
+  console.log(result(students, schoolId));
